223fa3ff92d986951e6f11fd6f3f2a6842090b70
Leonildo Bevilaqua <leonldobevilaqua@gmail.com>
2026-01-28 22:03:40 -0300

    fix(frontend): update vite proxy to use 127.0.0.1 to avoid ipv6 resolution issues on localhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,12 +10,12 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       proxy: {
         '/api': {
-          target: 'http://localhost:3005',
+          target: 'http://127.0.0.1:3005',
           changeOrigin: true,
           secure: false,
         },
         '/downloads': {
-          target: 'http://localhost:3005',
+          target: 'http://127.0.0.1:3005',
           changeOrigin: true,
           secure: false,
         }
